729deca8b494878832fa826031c0313f8754a509
benjaminlipnik <beni.lipnik@gmail.com>
2021-01-04 17:10:36 +0100

    Input capture with timer2 ch1.

--- main.c
+++ main.c
@@ -11,6 +11,41 @@ int putchar(int c) {
     return 0;
 }
 
+//TIM1
+//(CC1IF) je treba spucat v interruptu
+//TI1FP1 je input pin
+//CC1S bits are writable only when the channel is OFF (CC1E = 0 in TIM1_CCER1).
+
+//TIM2
+//PD4 -> TIM2 CH1
+
+volatile uint16_t delta = 0;
+volatile uint16_t tmp = 0;
+
+void input_capture() __interrupt(TIM2_CC_ISR) {
+  uint8_t end = TIM2_SR1 & _BV(1);//CC1IF
+  uint16_t ccr = TIM2_CCR1H << 8 | TIM2_CCR1L;
+
+  if(PD_IDR & _BV(4)) {
+    TIM2_CCER1 |= _BV(1);
+    tmp = ccr;
+  }
+  else {
+    delta = ccr - tmp;
+    TIM2_CCER1 &= ~_BV(1);
+  }
+}
+
+void timer2_init() {
+  TIM2_IER |= _BV(1); //CC1IE
+  TIM2_EGR |= _BV(1);//CC1G
+  TIM2_CCMR1 |= _BV(0) | (0b1111<<4); //4x filter, TI1FP1
+  TIM2_CCER1 |= _BV(0); //Enable capture
+  TIM2_PSCR |= 0b0100;
+
+  TIM2_CR1 |= _BV(TIM2_CR1_CEN) | _BV(TIM2_CR1_ARPE);
+}
+
 void timer4_init() {
   TIM4_CR1 |= _BV(0);//CEN
   TIM4_IER |= _BV(0); //UIE
@@ -21,9 +56,15 @@ void timer4_init() {
 int main () {
     CLK_CKDIVR = 0;//16mhz
     uart_init(9600);
+    timer2_init();
     enable_interrupts();
 
-    while(1) {
+    PD_DDR |= _BV(3);
+    PD_CR1 |= _BV(3); //OUTPUT
 
+    while(1) {
+      //util_delay_milliseconds(100);
+      printf("%u\n\r", delta);
+      //printf("Hello world!\n\r");
     }
 }
